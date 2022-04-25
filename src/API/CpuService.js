6f2fdb6025d3f353667b2c3f5bed8438f16cc153
dekol <dekolodkin@mail.com>
2022-04-25 19:35:52 +0300

    backend deploy to heroku, CpuService address to heroku

--- src/API/CpuService.js
+++ src/API/CpuService.js
@@ -2,22 +2,22 @@ import axios from 'axios';
 
 export default class CpuService {
     static async getAll() {
-        return await axios.get('http://localhost:8080/api/cpu');
+        return await axios.get('https://builderpc.herokuapp.com/api/cpu');
     }
 
     static async getById(id) {
-        return await axios.get('http://localhost:8080/api/cpu/' + id);
+        return await axios.get('https://builderpc.herokuapp.com/api/cpu/' + id);
     }
 
     static async create(object) {
-        return await axios.post('http://localhost:8080/api/cpu', object);
+        return await axios.post('https://builderpc.herokuapp.com/api/cpu', object);
     }
 
     static async updateById(id, object) {
-        return await axios.put('http://localhost:8080/api/cpu/' + id, object);
+        return await axios.put('https://builderpc.herokuapp.com/api/cpu/' + id, object);
     }
 
     static async deleteById(id) {
-        return await axios.delete('http://localhost:8080/api/cpu/' + id);
+        return await axios.delete('https://builderpc.herokuapp.com/api/cpu/' + id);
     }
 }
